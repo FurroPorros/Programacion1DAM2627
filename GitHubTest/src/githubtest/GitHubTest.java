7b0b8260c19f3d57a4bc4f15013ff5cabfbcf0c5
Alumno Tarde <Alumno Tarde@LB-DAW2-01.iesluisbraille.es>
2026-09-24 16:22:28 +0200

    Probamos con una primera modificacion

--- GitHubTest/src/githubtest/GitHubTest.java
+++ GitHubTest/src/githubtest/GitHubTest.java
@@ -15,6 +15,8 @@ public class GitHubTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hey que pasa mundo");
+        System.out.println("Probemos a modificar despues de comitear");
     }
     
 }
